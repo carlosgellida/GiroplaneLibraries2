8b7326b6ad0387076f7062c51aeb0ca7c22c309e
gellidao <gellidao@gmail.com>
2021-09-23 12:31:47 -0500

    better performance using RadioGamepad2 and RadioPayload2

---
+++ RadioGamepad2.h
@@ -0,0 +1,107 @@
+#include <SPI.h>
+#include "printf.h"
+#include "RF24.h"
+
+
+// instantiate an object for the nRF24L01 transceiver
+RF24 radio(14, 12); // using pin 14 for the CE pin, and pin 12 for the CSN pin
+
+// Let these addresses be used for the pair
+uint8_t address[][6] = {"1Node", "2Node"};
+
+// Used to control whether this node is sending or receiving
+bool role = false;  // true = TX role, false = RX role
+
+unsigned long timming ; 
+
+bool sended, recieved; 
+
+
+
+void InitialiceRadio() {
+
+  Matrix<4, 1> quaternio ; 
+      // initialize the transceiver on the SPI bus
+  if (!radio.begin()) {
+    Serial.println(F("radio hardware is not responding!!"));
+    while (1) {} // hold in infinite loop
+  }
+
+  int radioNumber = 0; 
+
+  radio.setPALevel(RF24_PA_MAX);  // RF24_PA_MAX is default.
+
+  radio.setRetries(1, 5); 
+
+  // save on transmission time by setting the radio to only transmit the
+  // number of bytes we need to transmit a float
+  radio.setPayloadSize(sizeof(quaternio)); // float datatype occupies 4 bytes
+
+  radio.setChannel(2475); 
+
+  radio.setDataRate(RF24_2MBPS); 
+
+  // set the TX address of the RX node into the TX pipe
+  radio.openWritingPipe(address[radioNumber]);     // always uses pipe 0
+
+  // set the RX address of the TX node into a RX pipe
+  radio.openReadingPipe(1, address[!radioNumber]); // using pipe 1
+
+  // additional setup specific to the node's role
+  if (role) {
+    radio.stopListening();  // put radio in TX mode
+  } else {
+    radio.startListening(); // put radio in RX mode
+  }
+  timming = micros(); 
+}
+
+bool send(Matrix<4, 1> qDeseado){
+  bool sended = false; 
+  if (role) {
+    bool report = radio.write(&qDeseado, sizeof(qDeseado)); 
+
+    if (report) {
+      Serial.print(F("Transmission successful! "));          // payload was delivered
+      Serial.print(F("current time = "));
+      Serial.print(timming);                 // print the timer result
+      Serial.println(F(" us. "));
+      role = !role  ;
+      radio.startListening();                                    // increment float payload
+      timming = micros(); 
+      sended = true; 
+    } else {
+      Serial.println(F("Transmission failed or timed out")); // payload was not delivered
+      role = !role; 
+      //InitialiceRadio();
+      radio.startListening();
+    }
+
+  } 
+  return sended; 
+} 
+
+
+bool recieve(Matrix<4, 1> &qCurrent){
+  bool recieved = false; 
+  if(!role) {
+    //String qCurrenString ; 
+    // This device is a RX node
+    uint8_t pipe;
+    timming = micros(); 
+    if (radio.available(&pipe)) {             // is there a payload? get the pipe number that recieved it
+      uint8_t bytes = radio.getPayloadSize(); // get the size of the payload
+    
+      radio.read(&qCurrent, bytes);            // fetch payload from FIFO
+      //qCurrent = str2arr(qCurrenString); 
+      Serial.print(F("Received "));
+      Serial.print(bytes);                    // print the size of the payload
+      Serial.print(F(" Bytes on pipe "));
+      Serial.println(pipe);                     // print the pipe number
+      role = !role  ;
+      recieved = true; 
+      radio.stopListening();
+    }
+  } // role
+  return recieved; 
+}
---
+++ RadioPayload2.h
@@ -0,0 +1,116 @@
+#include <SPI.h>
+#include "printf.h"
+#include "RF24.h"
+
+// instantiate an object for the nRF24L01 transceiver
+RF24 radio(14, 12); // using pin 7 for the CE pin, and pin 8 for the CSN pin
+
+// Let these addresses be used for the pair
+uint8_t address[][6] = {"1Node", "2Node"};
+bool radioNumber = 1; // 0 uses address[0] to transmit, 1 uses address[1] to transmit
+
+
+bool role = true;  // true = TX role, false = RX role
+float payload = 0.0;
+
+unsigned long timming ; 
+unsigned long time2; 
+
+bool sended, recieved ; 
+
+void InitialiceRadio() {
+    Matrix<4, 1> quaternio; 
+    if (!radio.begin()) {
+      Serial.println(F("radio hardware is not responding!!"));
+      while (1) {} // hold in infinite loop
+    }
+    radioNumber = 1; 
+
+    radio.setPALevel(RF24_PA_MAX);  // RF24_PA_MAX is default.
+
+    radio.setRetries(1, 6); 
+
+    // save on transmission time by setting the radio to only transmit the
+    // number of bytes we need to transmit a float
+
+    radio.setPayloadSize(sizeof(quaternio)); // float datatype occupies ?? bytes
+
+    radio.setChannel(2475); 
+
+    radio.setDataRate(RF24_2MBPS); 
+
+    // set the TX address of the RX node into the TX pipe
+    radio.openWritingPipe(address[radioNumber]);     // always uses pipe 0
+
+    // set the RX address of the TX node into a RX pipe
+    radio.openReadingPipe(1, address[!radioNumber]); // using pipe 1
+
+    // additional setup specific to the node's role
+    if (role) {
+      radio.stopListening();  // put radio in TX mode
+    } else {
+      radio.startListening(); // put radio in RX mode
+    }
+
+    timming = micros(); 
+    time2 = micros(); 
+}
+
+bool send(Matrix<4, 1> qCurrent){
+  bool sended = false; 
+
+    radio.stopListening();
+
+    bool report = radio.write(&qCurrent, sizeof(qCurrent));  
+
+    if (report) {
+      Serial.print(F("Transmission successful! "));          // payload was delivered
+      Serial.print(F("time taken to send: "));
+      Serial.print(micros() - time2);                 // print the timer result
+      time2 = micros(); 
+      Serial.print(F(" us.")); 
+      //Serial << qCurrent << '\n' ; 
+      radio.startListening();
+      sended = true; 
+    } else {
+      // Sí no puede transmitir reiniciar el radio
+      Serial.println(F("Transmission failed or timed out")); // payload was not delivered
+      radio.startListening();
+      //InitialiceRadio();
+    }
+
+  return sended; 
+}
+
+bool recieve(Matrix<4, 1> &qDeseado){
+  bool recieved = false; 
+
+  timming = micros(); 
+  while(true){
+    uint8_t pipe;
+    radio.startListening(); 
+    //Serial.println("listening"); 
+    if (radio.available(&pipe)) {             // is there a payload? get the pipe number that recieved it
+      uint8_t bytes = radio.getPayloadSize(); // get the size of the payload
+      radio.read(&qDeseado, bytes) ; 
+      Serial.print(F("Received "));
+      Serial.print(bytes);                    // print the size of the payload
+      Serial.print(F(" bytes on pipe "));
+      Serial.print(pipe);                     // print the pipe number
+      Serial.print("time taken to recieve: "); 
+      Serial.println(micros() - time2); 
+      time2 = micros(); 
+      recieved = true; 
+      radio.stopListening();
+      break ; 
+    }
+
+    if(micros() - timming > 8000){
+      Serial.println("time listening exceded!"); 
+      role = !role; 
+      break; 
+    }
+  }//while
+  
+  return recieved; 
+}
